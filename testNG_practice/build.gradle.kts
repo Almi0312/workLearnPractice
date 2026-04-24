7732ea9d36c4f14f4744f69d1a7d92c1c11901c2
minaev <alexmi031219@gmail.com>
2026-04-24 16:54:24 +0300

    testNG init

---
+++ testNG_practice/build.gradle.kts
@@ -0,0 +1,7 @@
+dependencies {
+    // Используем version catalog libs
+    testImplementation(libs.testng)
+    implementation(libs.aspectJ)
+    implementation(libs.spotbug)
+    implementation(libs.reflection)
+}
